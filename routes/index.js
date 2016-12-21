bde70b5a151574893daa307f399d7add5189dc49
ADKA1987 <eng.alaa87@yahoo.com>
2016-12-21 17:51:57 +0100

    Divid Dashboard into divs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ANSN' });
 });
 
 router.post('/dashboard',function (req, res) {
